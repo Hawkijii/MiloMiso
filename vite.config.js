8eca5138bc6a15484c47fb0ea85bfe9da8bc90bd
Hawkiji <ralphjay9guevarra@gmail.com>
2026-09-22 11:22:13 +0800

    Fix: add vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MiloMiso/', // Idagdag mo itong linya na ito bro (ito ang pangalan ng repository mo)
 })
